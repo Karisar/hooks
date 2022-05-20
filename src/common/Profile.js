c8139eb800a6b76ef3aa52dc901acc87c4e5bcce
Kari <kari.sarsila@gmail.com>
2022-05-20 14:20:11 +0300

    Some input fields

--- src/common/Profile.js
+++ src/common/Profile.js
@@ -1,5 +1,33 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 
 export default function () {
-  return <h1 style={{ marginTop: "10vh" }}>Profile</h1>;
+  return (
+    <Paper elevation={3}
+    style={{
+      width: 500,
+      padding: 50,
+      margin: 50
+    }}>
+
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+          width: 128,
+          height: 400,
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Firstname" variant="outlined" />
+        <TextField id="outlined-basic" label="Lastname" variant="outlined" />
+        <TextField id="outlined-basic" label="SSN" variant="outlined" />
+
+      </Box>
+    </Paper>
+
+  );
 }
