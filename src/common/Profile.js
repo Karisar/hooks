b057da21473016f444aa45edb5d4d698f85b893b
Kari <kari.sarsila@gmail.com>
2022-05-23 21:47:07 +0300

    component state with hooks

--- src/common/Profile.js
+++ src/common/Profile.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 
 export default function () {
+  const [firstname, setFirstname] = useState(null);
+  const [lastname, setLastname] = useState(null);
+  const [ssn, setSsn] = useState(null);
+
+
   return (
     <Paper elevation={3}
     style={{
@@ -22,10 +27,10 @@ export default function () {
         noValidate
         autoComplete="off"
       >
-        <TextField id="outlined-basic" label="Firstname" variant="outlined" />
-        <TextField id="outlined-basic" label="Lastname" variant="outlined" />
-        <TextField id="outlined-basic" label="SSN" variant="outlined" />
-
+        <TextField id="outlined-basic" label="Firstname" variant="outlined" onChange={e => setFirstname(e.target.value)}/>
+        <TextField id="outlined-basic" label="Lastname" variant="outlined" onChange={e => setLastname(e.target.value)}/>
+        <TextField id="outlined-basic" label="SSN" variant="outlined" onChange={e => setSsn(e.target.value)}/>
+        <p>firstname: {firstname}, lastname: {lastname}, ssn: {ssn}</p>
       </Box>
     </Paper>
 
